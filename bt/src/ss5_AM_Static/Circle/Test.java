3ed5368bcd5581b73c678a2ca4f20acf5ad89681
gtrng <giangtruongg179@gmail.com>
2023-08-21 21:07:33 +0700

    first commit

--- bt/src/ss5_AM_Static/Circle/Test.java
+++ bt/src/ss5_AM_Static/Circle/Test.java
@@ -1,2 +1,8 @@
-package ss5_AM_Static.Circle;public class Test {
+package ss5_AM_Static.Circle;
+
+public class Test {
+    public static void main(String[] args) {
+        Circle h = new Circle();
+        System.out.println(h.getArea());
+    }
 }
