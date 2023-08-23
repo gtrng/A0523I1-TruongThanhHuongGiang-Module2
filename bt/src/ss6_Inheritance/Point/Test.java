32186194a7710ac7b4bf30d19b3526ed585cbdf1
gtrng <giangtruongg179@gmail.com>
2023-08-23 20:21:20 +0700

    first commit

---
+++ bt/src/ss6_Inheritance/Point/Test.java
@@ -0,0 +1,10 @@
+package ss6_Inheritance.Point;
+
+public class Test {
+    public static void main(String[] args) {
+        Point2D m = new Point2D(3, 4);
+        Point3D n = new Point3D(4, 5,7);
+        System.out.println(m.toString());
+        System.out.println(n.toString());
+    }
+}
