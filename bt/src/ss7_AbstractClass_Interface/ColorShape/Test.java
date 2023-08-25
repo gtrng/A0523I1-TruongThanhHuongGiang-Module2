ac41877f7371e51f9ef1851485aad13a38f7b702
gtrng <giangtruongg179@gmail.com>
2023-08-25 18:55:36 +0700

    second commit

--- bt/src/ss7_AbstractClass_Interface/ColorShape/Test.java
+++ bt/src/ss7_AbstractClass_Interface/ColorShape/Test.java
@@ -2,7 +2,7 @@ package ss7_AbstractClass_Interface.ColorShape;
 
 public class Test {
     public static void main(String[] args) {
-        ShapeObject[] squares = {new Square(4), new Square(5), new Square(6)};
+        ShapeObject[] squares = {new Square(4), new Square(5), new Square(6), new Square(7)};
         for (int i =0; i < squares.length; i++){
             System.out.println("Square " +(i + 1));
             System.out.println("Area :" + squares[i].getArea());
